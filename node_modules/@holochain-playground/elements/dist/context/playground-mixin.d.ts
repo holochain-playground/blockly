4a4f130dcdda823b6ac84200f7665ade1184932e
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-04 18:46:27 +0100

    Updates

--- node_modules/@holochain-playground/elements/dist/context/playground-mixin.d.ts
+++
@@ -1,7 +0,0 @@
-import { Constructor } from 'lit-element';
-import { PlaygroundContext } from './context';
-export interface IPlaygroundElement extends PlaygroundContext {
-    updatePlayground(context: Partial<PlaygroundContext>): void;
-    showMessage(message: string): void;
-}
-export declare const PlaygroundMixin: <T extends Constructor<HTMLElement>>(baseClass: T) => T & Constructor<IPlaygroundElement>;
--- src/blocks/types.ts
+++
@@ -1,9 +0,0 @@
-export const ENTRY_HASH = 'EntryHash';
-export const HEADER_HASH = 'HeaderHash';
-export const PUB_KEY = 'AgentPubKey';
-export const ENTRY = 'Entry';
-export const ELEMENT = 'Element';
-export const AGENT_INFO = 'AgentInfo';
-export const ENTRY_DEF = 'EntryDef';
-
-export const colour = '#ffad42';
